41bc44bb5b09bca87ef4b3881fea078f1b86d4ca
Katie Nelson <katienelson@Katies-MacBook-Air-2.local>
2024-06-06 15:58:02 -0500

    ch15 studio

---
+++ SpaDay6/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpaDay6.ViewModels;
+
+public class AddUserViewModel
+{
+    [Required(ErrorMessage = "Username is required")]
+     
+    [StringLength (15, MinimumLength=5, ErrorMessage = "Username must be between 5 and 15 characters")]
+    public string? Username { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+
+    [StringLength (20, MinimumLength =6, ErrorMessage = "Password must be between 6 and 20 characters")]
+    public string? Password { get; set; }
+
+    [Required(ErrorMessage = "Verify password is required")]
+    
+    [StringLength (20, MinimumLength =6, ErrorMessage = "Password must be between 6 and 20 characters")]
+
+    public string? VerifyPassword { get; set; }
+    
+    [EmailAddress]
+    public string? Email { get; set; }
+
+}  
+
+
